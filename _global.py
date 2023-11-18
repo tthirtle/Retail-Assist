c53e0c4237e522690637e4ff724bde22554efba5
Thomas Thirtle <tthirtle@me.com>
2023-11-18 15:55:50 -0600

    changed global vars file

--- _global.py
+++ _global.py
@@ -1,3 +1,4 @@
+from PySimpleGUI import FILE_TYPES_ALL_FILES
 # Configuration options
 
 database_file = None
@@ -5,3 +6,10 @@ use_alt_description = True
 config_file = "config.ini"
 
 single_user_mode = True
+
+# File extensions
+
+excel_file_ext = (('Microsoft Excel 2007+ File','*.xlsx'),(FILE_TYPES_ALL_FILES[0]))
+csv_file_ext = (('Comma Separated Values','*.csv'),(FILE_TYPES_ALL_FILES[0]))
+pdf_file_ext = (("Adobe Acrobat File",'*.pdf'),(FILE_TYPES_ALL_FILES[0]))
+img_file_ext = (("All supported formats",'*.png *.jpg *.bmp'),(FILE_TYPES_ALL_FILES[0]))
